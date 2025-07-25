a4a24d24e6aeff6949f6456fde77e109091f224a
Webprojon <tokhirjonkhasanov@gmail.com>
2025-07-25 23:17:44 +0200

    Wipe funtion is added

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -40,7 +40,6 @@ const Resume = () => {
 			setImageUrl(imageUrl);
 
 			setFeedback(data.feedback);
-			console.log({ resumeUrl, imageUrl, feedback: data.feedback });
 		};
 
 		loadResume();
@@ -51,17 +50,19 @@ const Resume = () => {
 			<nav className="resume-nav">
 				<Link to="/" className="back-button">
 					<img src="/icons/back.svg" alt="logo" className="w-2.5 h-2.5" />
-					<span className="text-gray-800 text-sm font-semibold">Back to Home</span>
+					<span className="text-gray-800 text-sm font-semibold">Back to Homepage</span>
 				</Link>
 			</nav>
 			<div className="flex flex-row w-full max-lg:flex-col-reverse">
 				<section className="feedback-section bg-[url('/images/bg-small.svg')] bg-cover h-[100vh] sticky top-0 items-center justify-center">
-					{imageUrl && resumeUrl && (
+					{imageUrl && resumeUrl ? (
 						<div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
 							<a href={resumeUrl} target="_blank" rel="noopener noreferrer">
 								<img src={imageUrl} className="w-full h-full object-contain rounded-2xl" title="resume" />
 							</a>
 						</div>
+					) : (
+						<h2>No image</h2>
 					)}
 				</section>
 				<section className="feedback-section">
@@ -73,7 +74,7 @@ const Resume = () => {
 							<Details feedback={feedback} />
 						</div>
 					) : (
-						<img src="/images/resume-scan-2.gif" className="w-140p" />
+						<img src="/images/resume-scan-2.gif" className="w-full" />
 					)}
 				</section>
 			</div>
